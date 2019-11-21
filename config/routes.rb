c055532687f43b6f4c84488e0b362607f230907b
Chris Junker <elevatemedfit@gmail.com>
2019-11-21 14:18:53 -0500

    rebuilt sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'login', => 'sessions/new'
-  post 'login', => 'sessions/create'
-  get 'sessions/login'
-  get 'welcome', => 'sessions/welcome'
+  get 'login', => 'sessions#new'
+  post 'login', => 'sessions#create'
+  get 'sessions#login'
+  get 'welcome', => 'sessions#welcome'
+  get 'authorized', => 'session#page_requires_login'
   resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
